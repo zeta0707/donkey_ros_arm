af8b39422203712c176f36d029ceb7e2081cbcf8
Changwhan Lee <zeta0707@gmail.com>
2022-03-05 06:26:47 +0900

    Add csi_camera
    Add missing folder of csi_camera, it still need for webcam

---
+++ csi_camera/src/csi_pub.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+def gstreamer_pipeline(
+    capture_width=640,
+    capture_height=480,
+    display_width=640,
+    display_height=480,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+#for Waveshare jetracer csi camera orientation
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+rospy.init_node("csi_pub", anonymous=True)
+image_pub = rospy.Publisher("csi_image", Image, queue_size=1)
+
+bridge = CvBridge()
+
+while not rospy.is_shutdown():
+    # Capture frame-by-frame
+    ret, cv_image = cap.read()
+
+    # Display the resulting frame
+    # cv2.imshow('frame',cv_image)
+    # cv2.waitKey(3)
+    image_pub.publish(bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
---
+++ csi_camera/src/webcam_pub.cpp
@@ -0,0 +1,66 @@
+#include "csi_camera/VideoHandler.h"
+
+#include <stdlib.h> // for exit
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+
+// Webcam Image Publisher Class
+class WebcamPub {
+private:
+    VideoHandler *m_vh;
+    ros::NodeHandle m_nh;
+    // ros::Publisher m_image_pub;
+    image_transport::Publisher m_image_pub;
+
+    bool m_viz;
+
+    cv::Mat m_image;
+    sensor_msgs::ImagePtr m_msg;
+public:
+    WebcamPub(const int& device_ID_in = 0, const bool& viz_in = 0 ): m_viz(viz_in){
+
+        image_transport::ImageTransport it(m_nh);
+        m_image_pub = it.advertise("image_raw", 1);
+
+        m_vh = new VideoHandler(device_ID_in);
+    }
+
+    ~WebcamPub(){
+        delete m_vh;
+    }
+
+    void get_img(){
+        m_vh->get_frame(m_image);
+
+        // show live and wait for a key with timeout long enough to show images
+        if (m_viz){
+            cv::imshow("Live", m_image);
+            if (cv::waitKey(5) >= 0){
+                delete m_vh;
+                exit(1);
+            }
+        }
+    }
+
+    void pub_msg(){
+        get_img();
+        m_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", m_image).toImageMsg();
+        m_image_pub.publish(m_msg);
+    }
+};
+
+int main(int argc, char **argv){
+
+    ros::init(argc, argv, "webcam_pub");
+    WebcamPub my_pub(0);
+    ros::Rate r(10);
+
+    while( ros::ok() ){
+        my_pub.pub_msg();
+        r.sleep();
+    }
+
+    return 0;
+}
